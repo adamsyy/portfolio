3393c8fb6aed03f5a3d9c837d314116aee3e06b4
Asher Mathews <msasher123@gmail.com>
2021-08-07 21:04:31 +0530

    Added the watermark remover

--- watermark_remove.js
+++ watermark_remove.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log("I am ready");
     
-    setInterval(()=>{
+   let inter=setInterval(()=>{
         if( document.getElementsByClassName("w-webflow-badge").length!=0)
         {
         document.getElementsByClassName("w-webflow-badge")[0].remove();
         console.log("Gone Boom!!!!");
+        clearInterval(inter);
         }else{}
     },1000);
   })
